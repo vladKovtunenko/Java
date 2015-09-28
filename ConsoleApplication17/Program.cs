bd3e9c7525f47f83b9726ff862df182d977266f6
Владислав Ковтуненко <batyahim@outlook.com>
2015-09-28 18:15:19 +0300

    111

--- ConsoleApplication17/Program.cs
+++ ConsoleApplication17/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication17
     {
         static void Main(string[] args)
         {
-            string k =Console.ReadLine();
+            string bn =Console.ReadLine();
         }
     }
 }
